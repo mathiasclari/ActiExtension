39aed283285e774f3d991d3c03042505696d4908
Mat <silencekrakens@gmail.com>
2022-02-10 09:55:29 +0100

    Extension

---
+++ src/main/java/net/acticraft/actiextension/ActiExtension.java
@@ -0,0 +1,17 @@
+package net.acticraft.actiextension;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ActiExtension extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
