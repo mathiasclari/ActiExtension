fb0ebba2e9bb319417e43a71ed50bf1423458a4c
Mat <silencekrakens@gmail.com>
2022-02-10 23:58:28 +0100

    HelpMenuUpdate

--- src/main/java/net/acticraft/actiextension/DefaultCommands/HelpGuiCommand.java
+++ src/main/java/net/acticraft/actiextension/DefaultCommands/HelpGuiCommand.java
@@ -1,8 +1,10 @@
 package net.acticraft.actiextension.DefaultCommands;
 
+import net.acticraft.actiextension.GuiList.HelpGui;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
 public class HelpGuiCommand implements CommandExecutor {
@@ -10,6 +12,11 @@ public class HelpGuiCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(sender instanceof Player){
 
+
+            public void openInventory(final HumanEntity ent) {
+
+                ent.openInventory(HelpGui);
+            }
         }
         return false;
     }
